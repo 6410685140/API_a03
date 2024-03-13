d99bd762e2d8bb8793c9d2282e712719bd3dcf4f
Thanapat Sarathum <6410685140@cn331>
2024-03-13 14:48:53 +0700

    success API

---
+++ API_a03/API_a03App.swift
@@ -0,0 +1,17 @@
+//
+//  API_a03App.swift
+//  API_a03
+//
+//  Created by นายธนภัทร สาระธรรม on 6/3/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct API_a03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
